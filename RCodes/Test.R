a990f218e123ee12e2738ab00fba3b16518389b6
CarnoZhao <273806108@qq.com>
2019-07-13 15:10:16 +0800

    Linux Java test

--- RCodes/Test.R
+++ RCodes/Test.R
@@ -1,4 +1,7 @@
-x = rnorm(100, 0, 1)
-print(x)
-print(1)
+library(ggplot2)
+
+x = runif(10, 0, 1)
+y = as.factor(as.integer(10 * x))
+d = data.frame(x = x, y = y)
+ggplot(d, aes(theta = y, color = y, x = x, fill = y)) + coord_polar() + geom_bar()
 
