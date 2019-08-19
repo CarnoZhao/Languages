66c3d4a5973598a22e3f0ad0e5ae43aac76e24de
CarnoZhao <273806108@qq.com>
2019-08-19 11:20:02 +0800

    new julia notebook

--- JuliaCodes/Hello.jl
+++ JuliaCodes/Hello.jl
@@ -3,6 +3,51 @@ println("Hello world!");
 a = split("This is my first Julia code file!", ' ')
 
 for sub in a
-    println(sub)
+    print(sub)
+    print('_')
+end
+println()
+
+
+strings = "MyJulia"
+
+for char in strings
+    print(char)
+end
+println()
+
+strings = "1 + 1"
+println("1 + 1 = $(1 + 1)")
+
+α, β = 1, 2
+println(α + β)
+
+function MySum(A)
+    s = 0
+    for a in A
+        s += a
+    end
+    return s
 end
 
+list = [1 2 3 4 5]
+println(MySum(list))
+
+x1 = rand(2, 2)
+show(x1)
+println(x1)
+println(x1 .+ 2)
+
+struct MyComplex
+    rea::Float64
+    ima::Float64
+end
+a = MyComplex(1.0, 2.0)
+println(a.ima)
+
+import Base: *
+import Base: show
+show(x::MyComplex) = println("$(x.rea) + $(x.ima)im")
+*(a::MyComplex, b::MyComplex) = MyComplex(a.rea * b.rea - a.ima * b.ima, a.rea * b.ima + a.ima * b.rea)
+b = MyComplex(1, 2)
+show(a * b)
---
+++ JuliaCodes/linear_algebra.jl
@@ -0,0 +1,36 @@
+using LinearAlgebra
+using Random
+Random.seed!(0)
+
+A = rand(-3:3, 4, 4)
+
+function printa(A::Array)
+    l = size(A)[1]
+    for i = 1:l println(join(round.(A[i,:], digits = 2), ' ')) end
+    println()
+end
+
+β = rand(1:9, 4)
+
+printa(A)
+E₄ = Matrix{Int}(I, size(A))
+
+## A = QR, 正交矩阵Q，上三角矩阵R
+qr_result = qr(A)
+Q, R = Array(qr_result.Q), qr_result.R
+println("A = QR, \nQ × Qᵀ - E₄ = ")
+printa(round.(Q * Q' - E₄, digits = 1))
+
+## A求逆
+println("|A| = $(det(A))")
+inv_A = inv(A)
+println("A⁻¹ × A - E₄ =")
+printa(round.(inv_A * A - E₄, digits = 1))
+
+## 最小二乘解
+println("AX = β, \n, then X = ")
+X = 
+printa(A \ β)
+
+
+asdasdasd
