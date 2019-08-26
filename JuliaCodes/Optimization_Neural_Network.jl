1246a72965a7d727378cb60e2d4fa586c0bbcdb8
CarnoZhao <273806108@qq.com>
2019-08-26 16:30:28 +0800

    update ignore running optimization

--- JuliaCodes/Optimization_Neural_Network.jl
+++ JuliaCodes/Optimization_Neural_Network.jl
@@ -1,7 +1,7 @@
 using MLDatasets, LinearAlgebra, Statistics, Images, PyPlot, Random
 
-train_x, train_y = MNIST.traindata()
-test_x, test_y = MNIST.testdata()
+train_x, train_y = MNIST.traindata(Float64)
+test_x, test_y = MNIST.testdata(Float64)
 ;
 
 relu(x) = max(0, x)
@@ -27,7 +27,7 @@ function moment_initialization(parameters)
         vdb = zeros(size(b))
         push!(V, [vdW, vdb])
     end
-    S = copy(V)
+    S = deepcopy(V)
     V, S
 end
 
@@ -60,9 +60,9 @@ function backward(Y, parameters, caches, V, S, beta1, beta2)
     L = length(parameters)
     m = size(Y)[2]
     AL = caches[length(caches)]
-    dA = (AL - Y) ./ (AL .* (1 - AL))
+    dA = (AL - Y) ./ (AL .* (1 .- AL))
     for i in L:-1:1
-        A, A_prev, W = caches[i], caches[i - 1], parameters[i][1]
+        A, A_prev, W = caches[i + 1], caches[i], parameters[i][1]
         dZ = dA .* ifelse(i == L, sigmoid_derivative.(A), relu_derivative.(A))
         dW = dZ * A_prev' / m
         db = mean(dZ, dims = 2)
@@ -78,8 +78,8 @@ end
 function update_parameters(V, S, epsilon, parameters, learning_rate)
     L = length(parameters)
     for i in 1:L
-        parameters[i][1] -= learning_rate * V[i][1] ./ (sqrt(S[i][1]) .+ epsilon)
-        parameters[i][2] -= learning_rate * V[i][2] ./ (sqrt(S[i][2]) .+ epsilon)
+        parameters[i][1] -= learning_rate * V[i][1] ./ (sqrt.(S[i][1]) .+ epsilon)
+        parameters[i][2] -= learning_rate * V[i][2] ./ (sqrt.(S[i][2]) .+ epsilon)
     end
     parameters
 end
@@ -103,9 +103,9 @@ function network(X, Y, layer_dims; num_iterations = 3000, batch_size = 64, learn
         cost = 0
         batches = batch_data(X, Y, batch_size)
         for (mini_X, mini_Y) in batches
-            caches = forward(X, parameters)
+            caches = forward(mini_X, parameters)
             cost += compute_cost(caches[length(caches)], mini_Y)
-            V, S = backward(Y, parameters, caches, V, S)
+            V, S = backward(mini_Y, parameters, caches, V, S, beta1, beta2)
             parameters = update_parameters(V, S, epsilon, parameters, learning_rate)
         end
         if i % 100 == 0
@@ -115,14 +115,14 @@ function network(X, Y, layer_dims; num_iterations = 3000, batch_size = 64, learn
     parameters
 end
 
-function main(train_x, train_y, test_x, test_y; num_iterations = 3000, batch_size = 64, learning_rate = 0.0075, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-8)
-    train_x = reshape(train_x, :, size(train_x)[length(size(train_x))])
+function main(train_x, train_y, test_x, test_y; train_size = 6000, test_size = 100, num_iterations = 3000, batch_size = 64, learning_rate = 0.0075, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-8)
+    train_x = reshape(train_x, :, size(train_x)[length(size(train_x))])[:, 1:train_size]
     train_y = [ifelse(i == y, 1, 0) for y in train_y for i in 0:9]
-    train_y = reshape(train_y, 10, :)
-    test_x = reshape(test_x, :, size(test_x)[length(size(test_x))])
+    train_y = reshape(train_y, 10, :)[:, 1:train_size]
+    test_x = reshape(test_x, :, size(test_x)[length(size(test_x))])[:, 1:test_size]
     test_y = [ifelse(i == y, 1, 0) for y in test_y for i in 0:9]
-    test_y = reshape(test_y, 10, :)
-    layer_dims = [size(train_x)[1], 100, 50, 20, size(train_y)[1]]
+    test_y = reshape(test_y, 10, :)[:, 1:test_size]
+    layer_dims = [size(train_x)[1], 20, 15, size(train_y)[1]]
     parameters = network(train_x, train_y, layer_dims)
     pred_train_y = predict(train_x, parameters)
     pred_test_y = predict(test_x, parameters)
