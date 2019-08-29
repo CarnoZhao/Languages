8e522b9f722cc8f2615f224094b6c6a9898be2d8
CarnoZhao <273806108@qq.com>
2019-08-29 08:55:48 +0800

    finish optimization neural network

--- JuliaCodes/Optimization_Neural_Network.jl
+++ JuliaCodes/Optimization_Neural_Network.jl
@@ -1,13 +1,20 @@
-using CUDAdrv
-using CUDAnative
-using CuArrays
+use_gpu = false
+if use_gpu
+    using CUDAdrv
+    using CUDAnative
+    using CuArrays
+    cu = CuArrays.CuArray
+    expfunc = CUDAnative.exp
+else
+    cu = Array
+    expfunc = exp
+end
 using MLDatasets, LinearAlgebra, Statistics, Random
-cu = CuArrays.CuArray
 train_x, train_y = MLDatasets.MNIST.traindata(Float64)
 test_x, test_y = MLDatasets.MNIST.testdata(Float64)
 
 relu(x) = max(0, x)
-sigmoid(x) = 1 / (1 + CUDAnative.exp(-x))
+sigmoid(x) = 1 / (1 + expfunc(-x))
 relu_derivative(y) = ifelse(y > 0, 1, 0)
 sigmoid_derivative(y) = y * (1 - y)
 
@@ -59,7 +66,7 @@ function forward(X, parameters)
     caches
 end
 
-function backward(Y, parameters, caches, V, S, beta1, beta2, t)
+function backward(Y, parameters, caches, V, S, beta1, beta2)
     L = length(parameters)
     m = size(Y)[2]
     AL = caches[length(caches)]
@@ -70,19 +77,23 @@ function backward(Y, parameters, caches, V, S, beta1, beta2, t)
         dW = dZ * A_prev' / m
         db = mean(dZ, dims = 2)
         dA = W' * dZ
-        V[i][1] = (beta1 * V[i][1] + (1 - beta1) * dW) / (1 - beta1 ^ t)
-        V[i][2] = (beta1 * V[i][2] + (1 - beta1) * db) / (1 - beta1 ^ t)
-        S[i][1] = (beta2 * S[i][1] + (1 - beta2) * dW .^ 2) / (1 - beta2 ^ t)
-        S[i][2] = (beta2 * S[i][2] + (1 - beta2) * db .^ 2) / (1 - beta2 ^ t)
+        V[i][1] = beta1 * V[i][1] + (1 - beta1) * dW
+        V[i][2] = beta1 * V[i][2] + (1 - beta1) * db
+        S[i][1] = beta2 * S[i][1] + (1 - beta2) * dW .^ 2
+        S[i][2] = beta2 * S[i][2] + (1 - beta2) * db .^ 2
     end
     V, S
 end
 
-function update_parameters(V, S, epsilon, parameters, learning_rate)
+function update_parameters(V, S, epsilon, parameters, learning_rate, beta1, beta2, t)
     L = length(parameters)
     for i in 1:L
-        parameters[i][1] -= learning_rate * V[i][1] ./ (sqrt.(S[i][1]) .+ epsilon)
-        parameters[i][2] -= learning_rate * V[i][2] ./ (sqrt.(S[i][2]) .+ epsilon)
+        vW = V[i][1] / (1 - beta1 ^ t)
+        vb = V[i][2] / (1 - beta1 ^ t)
+        sW = S[i][1] / (1 - beta2 ^ t)
+        sb = S[i][2] / (1 - beta2 ^ t)
+        parameters[i][1] -= learning_rate * vW ./ (sqrt.(sW) .+ epsilon)
+        parameters[i][2] -= learning_rate * vb ./ (sqrt.(sb) .+ epsilon)
     end
     parameters
 end
@@ -106,29 +117,28 @@ function network(X, Y, layer_dims; num_iterations = 30000, batch_size = 64, lear
     parameters = random_initialization(layer_dims)
     V, S = moment_initialization(parameters)
     for i in 1:num_iterations
-        if batched
-            cost = 0
+        if batch_size != 0
             batches = batch_data(X, Y, batch_size)
             for (mini_X, mini_Y) in batches
                 caches = forward(mini_X, parameters)
-                cost += compute_cost(caches[length(caches)], mini_Y)
-                V, S = backward(mini_Y, parameters, caches, V, S, beta1, beta2, i)
-                parameters = update_parameters(V, S, epsilon, parameters, learning_rate)
+                V, S = backward(mini_Y, parameters, caches, V, S, beta1, beta2)
+                parameters = update_parameters(V, S, epsilon, parameters, learning_rate, beta1, beta2, i)
             end
         else
             caches = forward(X, parameters)
-            cost = compute_cost(caches[length(caches)], Y)
-            V, S = backward(Y, parameters, caches, V, S, beta1, beta2, i)
-            parameters = update_parameters(V, S, epsilon, parameters, learning_rate)
+            V, S = backward(Y, parameters, caches, V, S, beta1, beta2)
+            parameters = update_parameters(V, S, epsilon, parameters, learning_rate, beta1, beta2, i)
         end
         if i % 100 == 0
+            caches = forward(X, parameters)
+            cost = compute_cost(caches[length(caches)], Y)
             println("Cost after $(i) iterations: $(cost)")
         end
     end
     parameters
 end
 
-function main(train_x, train_y, test_x, test_y; train_size = 60000, test_size = 10000, num_iterations = 30000, batch_size = 64, learning_rate = 0.0007, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-8, batched = true)
+function main(train_x, train_y, test_x, test_y; train_size = 60000, test_size = 10000, num_iterations = 30000, batch_size = 64, learning_rate = 0.0007, beta1 = 0.9, beta2 = 0.999, epsilon = 1e-8)
     train_x = reshape(train_x, :, size(train_x)[length(size(train_x))])[:, 1:train_size]
     train_y_num = train_y[1:train_size]
     train_y = [ifelse(i == y, 1, 0) for y in train_y for i in 0:9]
@@ -138,7 +148,7 @@ function main(train_x, train_y, test_x, test_y; train_size = 60000, test_size = 
     test_y = [ifelse(i == y, 1, 0) for y in test_y for i in 0:9]
     test_y = reshape(test_y, 10, :)[:, 1:test_size]
     layer_dims = [size(train_x)[1], 20, 15, size(train_y)[1]]
-    parameters = network(train_x, train_y, layer_dims, num_iterations = num_iterations, batch_size = batch_size, learning_rate = learning_rate, beta1 = beta1, beta2 = beta2, epsilon = epsilon, batched = batched)
+    parameters = network(train_x, train_y, layer_dims, num_iterations = num_iterations, batch_size = batch_size, learning_rate = learning_rate, beta1 = beta1, beta2 = beta2, epsilon = epsilon)
     pred_train_y = predict(train_x, parameters)
     pred_test_y = predict(test_x, parameters)
     println("Train accuracy: $(round(mean(pred_train_y .== train_y_num) * 100, digits = 3))%")
@@ -146,4 +156,4 @@ function main(train_x, train_y, test_x, test_y; train_size = 60000, test_size = 
     parameters
 end
 
-parameters = main(train_x, train_y, test_x, test_y, train_size = 600, test_size = 100, num_iterations = 500, learning_rate = 0.0007, batched = false);
+parameters = main(train_x, train_y, test_x, test_y, train_size = 2000, test_size = 600, num_iterations = 1000, learning_rate = 0.0007, beta1 = 0.9, beta2 = 0.99999, epsilon = 1e-8, batch_size = 256);
